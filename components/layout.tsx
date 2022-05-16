e39373712777fe7a714754e52576161cb7f23bc8
Jan <42362229+jangarong@users.noreply.github.com>
2022-05-15 21:16:58 -0400

    Add footer.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Footer from "./footer";
 import Navbar from "./navbar";
 
 export default function Layout({ children }: {children: React.ReactNode}) {
@@ -11,6 +12,7 @@ export default function Layout({ children }: {children: React.ReactNode}) {
       </Head>
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </div>
   )
 }
