a59f65b88233e7da34904b7bc41d56c96e9838fa
Jan Garong <jan.garong@mail.utoronto.ca>
2024-11-26 19:36:57 -0500

    darkness

--- src/components/jang-card-mobile.tsx
+++ src/components/jang-card-mobile.tsx
@@ -19,29 +19,31 @@ export function JangCardMobile() {
                 className="flex w-[350px] h-[650px] flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl"
                 color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
             >
-                <div className="flex flex-col">
+                <div className="bg-lightblack flex flex-row items-center justify-center mt-[-20px] mb-[-20px] overflow-hidden">
+                    <div className="flex flex-col">
 
-                    <div className="flex flex-row items-center justify-between ml-6 mr-6 text-lg mt-[20px]">
-                        <CardTitle>Jan Garong</CardTitle>
-                        <div className="text-xs items-center flex flex-row justify-end">
-                            <CodeIcon className="ml-2 mr-2" height={40} />
-                            <DownloadIcon className="ml-2 mr-2" height={40} />
+                        <div className="flex flex-row items-center justify-between ml-6 mr-6 text-lg mt-[20px]">
+                            <CardTitle>Jan Garong</CardTitle>
+                            <div className="text-xs items-center flex flex-row justify-end">
+                                <CodeIcon className="ml-2 mr-2" height={40} />
+                                <DownloadIcon className="ml-2 mr-2" height={40} />
+                            </div>
                         </div>
+                        <Card className="mt-[15px] w-[350px] h-[425px] rounded-none">
+                            <div className="opacity-10 w-[100%] h-[100%] bg-[url('/jang45.svg')] dark:invert bg-repeat-space bg-[length:40px_40px]"></div>
+                        </Card>
+                        <CardContent className="mt-4">
+                            <CardDescription className="text-center mb-2">
+                                <AnimatedShinyText className="inline-flex items-center justify-center px-4 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+                                    Developing Cybersecurity tools at
+                                </AnimatedShinyText>
+                            </CardDescription>
+                            <TeamsContent isMobile={true} />
+                            <div className="flex w-[100%] text-xxs text-center justify-center">
+                                *Starting January 2025
+                            </div>
+                        </CardContent>
                     </div>
-                    <Card className="mt-[15px] w-[350px] h-[425px] rounded-none">
-                        <div className="opacity-10 w-[100%] h-[100%] bg-[url('/jang45.svg')] dark:invert bg-repeat-space bg-[length:40px_40px]"></div>
-                    </Card>
-                    <CardContent className="mt-4">
-                        <CardDescription className="text-center mb-2">
-                            <AnimatedShinyText className="inline-flex items-center justify-center px-4 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
-                                Developing Cybersecurity tools at
-                            </AnimatedShinyText>
-                        </CardDescription>
-                        <TeamsContent isMobile={true} />
-                        <div className="flex w-[100%] text-xxs text-center justify-center">
-                            *Starting January 2025
-                        </div>
-                    </CardContent>
                 </div>
             </ShineBorder>
         </Card>
--- src/components/jang-card.tsx
+++ src/components/jang-card.tsx
@@ -21,45 +21,47 @@ export function JangCard() {
                 className="flex w-[650px] h-[350px] flex-row items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl"
                 color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
             >
-                <Card className="ml-[40px] w-[275px] h-[350px] rounded-none">
-                    <div className="opacity-10 w-[100%] h-[100%] bg-[url('/jang45.svg')] dark:invert bg-repeat-space bg-[length:40px_40px]"></div>
-                </Card>
-                <div className="flex flex-col items-center">
-
-                    <CardHeader>
-                        <div className="flex flex-col items-center justify-between pt-4 text-xl">
-                            <CardTitle className="p-2">
-                                Jan Garong
-                            </CardTitle>
-                            <CardDescription className="text-center">
-                                <AnimatedShinyText className="inline-flex items-center justify-center px-4 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
-                                    <span>Developing Cybersecurity tools at</span>
-                                </AnimatedShinyText>
-                            </CardDescription>
-                        </div>
-                    </CardHeader>
-                    <CardContent className="w-[90%] mt-[-8px]">
-                        <TeamsContent />
-                        <div className="flex w-[100%] mt-[20px] text-xxs text-center justify-center">
-                            *Starting January 2025
-                        </div>
-                    </CardContent>
-                    <CardFooter className="flex justify-between w-[100%]">
-                        <a href="/works">
-                            <Button variant="outline">
-                                Contributions
-                                <CodeIcon />
-                            </Button>
-                        </a>
-                        <a href="/resume.pdf">
-                            <Button>
-                                Resume
-                                <DownloadIcon />
-                            </Button>
-                        </a>
-                    </CardFooter>
+                <div className="bg-lightblack flex flex-row items-center justify-center ml-[-20px] mr-[-10px] overflow-hidden">
+                    <Card className="ml-[40px] w-[275px] h-[350px] rounded-none">
+                        <div className="opacity-10 w-[100%] h-[100%] bg-[url('/jang45.svg')] dark:invert bg-repeat-space bg-[length:40px_40px]"></div>
+                    </Card>
+                    <div className="flex flex-col items-center">
+                        <CardHeader>
+                            <div className="flex flex-col items-center justify-between pt-4 text-xl">
+                                <CardTitle className="p-2">
+                                    Jan Garong
+                                </CardTitle>
+                                <CardDescription className="text-center">
+                                    <AnimatedShinyText className="inline-flex items-center justify-center px-4 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+                                        <span>Developing Cybersecurity tools at</span>
+                                    </AnimatedShinyText>
+                                </CardDescription>
+                            </div>
+                        </CardHeader>
+                        <CardContent className="w-[90%] mt-[-8px]">
+                            <TeamsContent />
+                            <div className="flex w-[100%] mt-[20px] text-xxs text-center justify-center">
+                                *Starting January 2025
+                            </div>
+                        </CardContent>
+                        <CardFooter className="flex justify-between w-[100%]">
+                            <a href="/works">
+                                <Button variant="outline">
+                                    Contributions
+                                    <CodeIcon />
+                                </Button>
+                            </a>
+                            <a href="/resume.pdf">
+                                <Button>
+                                    Resume
+                                    <DownloadIcon />
+                                </Button>
+                            </a>
+                        </CardFooter>
+                    </div>
                 </div>
             </ShineBorder>
+
         </Card >
     )
 }
