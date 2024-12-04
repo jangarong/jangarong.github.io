da9455f54f7c7400cce01039453b02c4221b843c
Jan Garong <jan.garong@mail.utoronto.ca>
2024-12-04 18:25:48 -0500

    Remove top padding.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div>
       <div className="bkg__spotlight w-[100%] h-[100%] min-h-[640px] overflow-auto"></div>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-20 gap-16 md:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-20 md:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start pb-[64px]">
           <div className="hidden md:block">
             <JangCard />
           </div>
