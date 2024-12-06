32a73d927ec089d3ada844c8a115b30c730467f8
Jan Garong <jan.garong@mail.utoronto.ca>
2024-12-06 11:53:46 -0500

    fix hiding first card

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -8,7 +8,7 @@ export default function Works() {
     <div className="flex flex-col min-h-[640px]">
       <div className="bkg__spotlight w-[100%] h-[100%] min-h-[640px] overflow-auto"></div>
       <WorksHeader />
-      <main className="absolute flex flex-col gap-8 row-start-2 items-start xl:items-center flex-grow w-[100%]">
+      <main className="absolute flex flex-col gap-8 row-start-2 items-start works-xl:items-center flex-grow w-[100%]">
         <div className="mt-[172px]">
           <TeamCarousel />
         </div>
