30fc80a8e7e7d577e44b370a19e38c0aa3832ebc
Jan Garong <jan.garong@mail.utoronto.ca>
2024-12-05 20:46:01 -0500

    Add background images to the cards.

--- src/components/team-card.tsx
+++ src/components/team-card.tsx
@@ -5,7 +5,7 @@ import { ArrowRightIcon } from "lucide-react";
 
 interface TeamProps {
     avatar: string;
-    // teamLogo: string; // TODO: Hack; teamLogo is CSS className
+    teamLogo: string; // TODO: Hack; teamLogo is CSS className
     description: string;
     team: string;
     date: string;
@@ -14,10 +14,10 @@ interface TeamProps {
     invert?: boolean;
 }
 
-export function TeamCard({ avatar, description, team, date, url, color, invert }: TeamProps) {
+export function TeamCard({ avatar, description, team, date, url, color, invert, teamLogo }: TeamProps) {
     return (
         <Card className={`border-${color} bg-${color} h-[520px] w-[320px] rounded-3xl flex flex-col`}>
-            <CardContent className="flex-grow">
+            <CardContent className="flex-grow z-10">
                 <div className="flex gap-3 flex-row items-center mb-[16px] mt-[16px] mr-[16px]">
                     <Avatar>
                         <AvatarImage src={avatar} />
@@ -32,12 +32,13 @@ export function TeamCard({ avatar, description, team, date, url, color, invert }
                     <p>{description}</p>
                 </div>
             </CardContent>
-            <CardFooter className={`bottom-0 left-0 right-0 flex justify-end ${invert ? 'invert' : ''}`}>
+            <CardFooter className={`z-10 bottom-0 left-0 right-0 flex justify-end ${invert ? 'invert' : ''}`}>
                 <Link href={url} target="_blank"
                     rel="noopener noreferrer">
                     <ArrowRightIcon />
                 </Link>
             </CardFooter>
+            <Card className={`absolute border-${color} bg-${color} ${teamLogo} h-[520px] w-[320px] rounded-3xl flex flex-col opacity-30`}></Card>
         </Card>
     )
 }
--- src/components/team-carousel.tsx
+++ src/components/team-carousel.tsx
@@ -9,14 +9,14 @@ export default function TeamCarousel() {
             "description": "Coming soon...",
             "url": "https://cloud.google.com/security/products/security-command-center?hl=en",
             "color": "white",
-            "teamLogo": "/teams/",
+            "teamLogo": "card-scc",
             "invert": true
         },
         {
             "name": "Email Security",
             "date": "Jan 2024 - Dec 2024",
             "avatar": "/teams/trendmicro.png",
-            "teamLogo": "/teams/",
+            "teamLogo": "card-tmems",
             "description": "Currently developing backend log features + development tools for the Email Security product. This product aims to prevent incoming malicious emails such as those that contain phishing, ransomware, spam, as well as other protocol filters like IP reputation, spoofing, etc.",
             "url": "https://www.trendmicro.com/en_ca/business/products/user-protection/sps/email-and-collaboration/email-security.html",
             "color": "trendmicro"
@@ -25,7 +25,7 @@ export default function TeamCarousel() {
             "name": "CylanceMDR",
             "date": "Sept 2023 - Dec 2023",
             "avatar": "/teams/blackberry.jpg",
-            "teamLogo":"/teams/",
+            "teamLogo": "card-cylancemdr",
             "description": "An MDR and threat intelligence platform for incoming malicious events. Monitored on devices with Cylance's AI, and managed by security analysts available 24/7. Previously developed microservices used to maintain the data pipeline, as well as APIs that are used for multi-tenancy and showing alerts detected on client endpoints.",
             "url": "https://docs.blackberry.com/en/unified-endpoint-security/cylancemdr",
             "color": "blackberry"
@@ -34,7 +34,7 @@ export default function TeamCarousel() {
             "name": "Skynet",
             "date": "May 2022 - Aug 2022",
             "avatar": "/teams/certik.jpg",
-            "teamLogo":"/teams/",
+            "teamLogo": "card-skynet",
             "description": "An all-in-one security analytics platform that gives insights to a variety of Web3 projects. This includes on-chain data, visualizations, security features, project activity, and many more! Worked on developing many of the widgets, most notably the bug bounty feature.",
             "url": "https://www.certik.com/products/skynet",
             "color": "neutral-900"
@@ -43,7 +43,7 @@ export default function TeamCarousel() {
             "name": "Zodiac Tail",
             "date": "July 2023 - Sept 2023",
             "avatar": "/teams/zodiactail.png",
-            "teamLogo":"/teams/",
+            "teamLogo": "card-zodiactail",
             "description": "A puzzle platformer with time mechanics that features the Zodiac Animals! I led development while Amy Li led the art direction for this game!",
             "url": "/zodiactail/index.html",
             "color": "orange-500"
@@ -60,7 +60,7 @@ export default function TeamCarousel() {
                         url={team.url}
                         color={team.color}
                         invert={team.invert}
-                    // teamLogo={team.teamLogo}
+                        teamLogo={team.teamLogo}
                     />
                 </div>)}
         </div>
