e214dcc8e99c7df5e9830b26cda37b9689fc2912
Jan Garong <jan.garong@mail.utoronto.ca>
2024-12-10 00:58:37 -0500

    Add resume back to card header

--- src/components/portal-card-mobile-header.tsx
+++ src/components/portal-card-mobile-header.tsx
@@ -1,5 +1,5 @@
 import { CardTitle, CardDescription } from "@/components/ui/card";
-import { HomeIcon } from "lucide-react";
+import { FileTextIcon, HomeIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function PortalCardMobileHeader() {
@@ -14,6 +14,9 @@ export default function PortalCardMobileHeader() {
             <div className="text-xs items-center flex flex-row justify-between">
                 <Link href="/">
                     <HomeIcon className="ml-4 opacity-50" />
+                </Link>
+                <Link href="/resume.pdf">
+                    <FileTextIcon className="ml-4 opacity-50" />
                 </Link>
             </div>
         </div>
