cef71e1fe7f404b950aba15d16acc5096c5216ff
Jan Garong <jan.garong@mail.utoronto.ca>
2024-12-15 16:57:30 -0500

    add technology marquee

--- src/components/technologies-marquee.tsx
+++ src/components/technologies-marquee.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import Marquee from "@/components/ui/marquee";
+import Image from "next/image";
 
 const reviews = [
   {
@@ -78,17 +79,120 @@ const ReviewCard = ({
   );
 };
 
+const techImgs = [
+  {
+    src: "/technologies/next.svg",
+    alt: "next.js",
+    width: 150,
+    height: 40,
+  },
+  {
+    src: "/technologies/react.svg",
+    alt: "react",
+    width: 50,
+    height: 50
+  },
+  {
+    src: "/technologies/unity.png",
+    alt: "unity",
+    width: 150,
+    height: 50
+  },
+  {
+    src: "/technologies/sodility.svg",
+    alt: "sodility",
+    width: 50,
+    height: 25
+  },
+  {
+    src: "/technologies/lambda.svg",
+    alt: "lambda",
+    width: 50,
+    height: 50
+  },
+  {
+    src: "/technologies/docker.svg",
+    alt: "docker",
+    width: 200,
+    height: 50
+  },
+  {
+    src: "/technologies/js.svg",
+    alt: "js",
+    width: 50,
+    height: 50
+  },
+  {
+    src: "/technologies/java.svg",
+    alt: "java",
+    width: 40,
+    height: 50
+  },
+  {
+    src: "/technologies/golang.svg",
+    alt: "golang",
+    width: 100,
+    height: 50
+  },
+  {
+    src: "/technologies/ts.svg",
+    alt: "ts",
+    width: 50,
+    height: 50
+  },
+  {
+    src: "/technologies/kubernetes.svg",
+    alt: "k8s",
+    width: 200,
+    height: 75
+  },
+  {
+    src: "/technologies/aws.webp",
+    alt: "aws",
+    width: 100,
+    height: 50
+  },
+  {
+    src: "/technologies/gcp.svg",
+    alt: "gcp",
+    width: 50,
+    height: 50
+  },
+  {
+    src: "/technologies/otel.svg",
+    alt: "otel",
+    width: 150,
+    height: 50
+  },
+  {
+    src: "/technologies/terraform.svg",
+    alt: "terraform",
+    width: 100,
+    height: 100
+  },
+  {
+    src: "/technologies/elastic.svg",
+    alt: "elastic",
+    width: 100,
+    height: 50
+  },
+];
+
 export function TechnologiesMarquee() {
   return (
-    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+    <div className="relative flex h-[200px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <Marquee pauseOnHover className="[--duration:20s]">
-        {firstRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+        {techImgs.slice(0, techImgs.length/2).map((imgProps) => (
+          <div key={imgProps.alt} className="flex flex-col justify-center h-[100%] m-2">
+            <Image {...imgProps} />
+          </div>
         ))}
       </Marquee>
-      <Marquee reverse pauseOnHover className="[--duration:20s]">
-        {secondRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+      <Marquee reverse pauseOnHover className="[--duration:20s] flex justify-center align-center">
+        {techImgs.slice(techImgs.length/2).map((imgProps) => (
+          <div key={imgProps.alt} className="flex flex-col justify-center h-[100%] m-2">
+            <Image {...imgProps} />
+          </div>
         ))}
       </Marquee>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
