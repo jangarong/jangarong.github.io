634c12e1dc5cd9c31e3f7faf6a7d349d4f34a608
Jan Garong <jan.garong@mail.utoronto.ca>
2024-12-16 21:26:37 -0500

    reformat and add proper hero

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -7,14 +7,17 @@ export default function Home() {
     <div className="">
       <JangHero />
       <div className="">
-        <div className="flex flex-col items-center justfiy-center">
-          <div className="text-6xl font-bold mb-[64px]">Featured Works</div>
-          <div className="w-[80%] max-w-[1024px] mb-[64px]"><FeaturedBento /></div>
-          <div className="my-[32px] text-center">
-            <div className="text-6xl font-bold mb-[8px]">Foundations</div>
-            <div className="text-xl opacity-50">software design is my passion</div>
-          </div>
+        <div className="my-[64px] text-center">
+          <div className="text-3xl md:text-6xl font-bold mb-[8px]">Tools & Frameworks</div>
+          <div className="text-md md:text-xl opacity-50">This is what I use to build everything!</div>
           <TechnologiesMarquee />
+        </div>
+        {/* <TechnologiesMarquee /> */}
+        <div className="flex flex-col items-center justfiy-center">
+          <div className="text-3xl md:text-6xl font-bold mb-[32px] md:mb-[64px] text-center">Contributions</div>
+          <div className="w-[80vw] mb-[64px]">
+            <FeaturedBento />
+          </div>
         </div>
       </div>
     </div>
