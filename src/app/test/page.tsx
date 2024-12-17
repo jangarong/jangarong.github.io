727a916e2aeda4bf54d3061ecf47880d40930761
Jan Garong <jan.garong@mail.utoronto.ca>
2024-12-16 22:55:21 -0500

    fix warnings

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             <FeaturedBento />
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center mt-[128px]">
+        <div className="flex flex-col items-center justify-center my-[128px]">
           <div className="text-3xl md:text-5xl font-bold mb-[32px] w-[80vw] text-left">
             <div className="w-[75%] md:w-[50%] leading-normal">
               Built with the best tools possible.
--- src/components/jang-hero.tsx
+++ src/components/jang-hero.tsx
@@ -7,16 +7,16 @@ export default function JangHero() {
                 <div className="hidden md:block">
                     <div className="flex justify-between items-end mb-[64px]">
                         <Image src={"/jang.svg"} alt={"Jan G."} width={150} height={50} className="dark:invert" />
-                        <p className="text-2xl">Cybersecurity's Software Developer</p>
+                        <p className="text-2xl">Cybersecurity&apos;s Software Developer</p>
                     </div>
-                    <div className="rounded-[64px] w-[80vw] h-[680px] bg-[url('/screenshots/utsc-desktop.jpeg')] bg-cover bg-center bg-no-repeat"></div>
+                    <div className="rounded-[64px] w-[80vw] h-[760px] bg-[url('/screenshots/utsc-desktop.jpeg')] bg-cover bg-center bg-no-repeat"></div>
                 </div>
                 <div className="block md:hidden">
                 <div className="flex flex-col justify-between items-center mb-[48px]">
                         <Image src={"/jang.svg"} alt={"Jan G."} width={100} height={25} className="dark:invert" />
-                        <p className="text-md mt-[16px]">Cybersecurity's Software Developer</p>
+                        <p className="text-md mt-[16px]">Cybersecurity&apos;s Software Developer</p>
                     </div>
-                    <div className="rounded-[32px] w-[80vw] h-[680px] bg-[url('/screenshots/utsc-mobile.jpeg')] bg-cover bg-center bg-no-repeat"></div>
+                    <div className="rounded-[32px] w-[80vw] h-[760px] bg-[url('/screenshots/utsc-mobile.jpeg')] bg-cover bg-center bg-no-repeat"></div>
                 </div>
                 {/* <div className="mt-[10px] text-xl md:text-3xl font-bold w-[350px] md:w-[650px] text-center opacity-75">a.k.a. Jan Garong</div>
                 <div className="mt-[10px] md:mt-[25px] text-md md:text-xl w-[300px] md:w-[650px] text-center opacity-50">
