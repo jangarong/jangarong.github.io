6e848e51077c0af4d0c561eef0c6b47679fb1964
Jan Garong <jan.garong@mail.utoronto.ca>
2024-12-18 13:18:40 -0500

    remove footer extra padding

--- src/components/linktree-footer.tsx
+++ src/components/linktree-footer.tsx
@@ -5,7 +5,7 @@ import { FileTextIcon, GithubIcon, KeyRoundIcon, LinkedinIcon, MailIcon } from "
 
 export default function LinktreeFooter() {
     return (
-        <div className="flex flex-col items-center w-[100%] pb-[64px]">
+        <div className="flex flex-col items-center w-[100%]">
             <Separator className="my-4 w-[80vw]" />
             <div className="flex flex-col justify-center h-[267px] md:h-[168px] w-[80vw] opacity-50">
                 <div className="flex gap-8 justify-center flex-wrap pl-[24px] pr-[24px]">
