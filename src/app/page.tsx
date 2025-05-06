9a86f36f99ca6950ba00a49087838d48dc9df19e
Jan Garong <jan.garong@mail.utoronto.ca>
2025-05-05 19:32:24 -0700

    add old card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Experience } from "@/components/experience";
 import { InfoMail } from "@/components/info-mail";
 import { TheWorld } from "@/components/the-world";
 import "../app/globals.css"
+import RGBCardDesktop from "@/components/rgb-card-desktop";
+import JangCardDesktopContent from "@/components/jang-card-desktop-content";
 
 export default function Home() {
   return (
@@ -18,6 +20,11 @@ export default function Home() {
         </div>
         <div className="pb-[128px] flex justify-center">
           <InfoMail />
+        </div>
+        <div className="pb-[128px] flex justify-center">
+        <RGBCardDesktop width={650} height={350}>
+          <JangCardDesktopContent />
+        </RGBCardDesktop>
         </div>
         <footer className="bg-black text-white p-12 text-center">
         website code and design by me
