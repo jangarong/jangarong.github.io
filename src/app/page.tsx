1993e34438397539636f2c7a2dbea51e6ab06e19
Jan Garong <jan.garong@mail.utoronto.ca>
2025-05-05 21:24:58 -0700

    refactor.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import "../app/globals.css"
 import RgbBusinessCard from "@/components/rgb-business-card";
 import { Button } from "@/components/ui/button";
 import { FileTextIcon } from "lucide-react";
+import DownloadMyResume from "@/components/download-my-resume";
 
 export default function Home() {
   return (
@@ -19,15 +20,7 @@ export default function Home() {
           <Experience />
         </div>
         <div className="pb-[128px] flex justify-center">
-          <div className="flex flex-col justify-center items-center">
-            <div className="text-center mb-4 text-xl font-bold">Want to learn more about my work?</div>
-            <div className="text-center mb-8 text-lg">Download my free resume!</div>
-            <a href="/resume.pdf">
-              <Button className="w-[300px] text-2xl font-bold p-8" >
-                <FileTextIcon style={{height: '22px', width: '22px'}} /> Download
-              </Button>
-            </a>
-          </div>
+          <DownloadMyResume />
         </div>
         <div className="pb-[128px] flex justify-center">
           <RgbBusinessCard />
---
+++ src/components/download-my-resume.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { FileTextIcon } from "lucide-react";
+
+export default function DownloadMyResume() {
+  return (
+
+          <div className="flex flex-col justify-center items-center">
+            <div className="text-center mb-4 text-xl font-bold">Want to learn more about my work?</div>
+            <div className="text-center mb-8 text-lg">Download my free resume!</div>
+            <a href="/resume.pdf">
+              <Button className="w-[300px] text-2xl font-bold p-8" >
+                <FileTextIcon style={{height: '22px', width: '22px'}} /> Download
+              </Button>
+            </a>
+          </div>
+  );
+}
