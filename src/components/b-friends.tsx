9fde4703cfed66ad9a8bcbd75ef10f81e445be34
Jan Garong <jan.garong@mail.utoronto.ca>
2025-05-31 20:25:37 -0700

    complete the list

--- src/components/b-friends.tsx
+++ src/components/b-friends.tsx
@@ -23,17 +23,24 @@ function FriendItem({ url, img, name }: Friend) {
 
 const frens: Friend[] = [
     {
-        url: "",
+        url: "https://www.linkedin.com/in/meixuan-mexi-lu-5103621b6/",
+        img: "/assets/b/friends/meixuan.jpg",
+        name: "Meixuan Lu"
+    },
+    {
+        url: "https://www.linkedin.com/in/trinity-li/",
+        img: "/assets/b/friends/trinity.jpg",
         name: "Trinity Li"
     },
     {
-        url: "",
+        url: "https://jcaoj.github.io/",
+        img: "/assets/b/friends/jay.jpg",
         name: "Jess Cao"
     },
     {
-        "url": "https://epicsteve2.github.io/",
-        "img": "/assets/b/friends/stephen.jpg",
-        "name": "Stephen Guo"
+        url: "https://epicsteve2.github.io/",
+        img: "/assets/b/friends/stephen.jpg",
+        name: "Stephen Guo"
     },
     {
         url: "https://liamy.webflow.io/",
@@ -60,17 +67,18 @@ const frens: Friend[] = [
         name: "Vivek Kandathil",
         img: "/assets/b/friends/vivek.jpg"
     }
-]
+].reverse()
 
 export default function BFriends() {
     return (
         <div className="flex justify-end w-[100%]">
             <div className={"w-[85%] rounded-tl-2xl rounded-bl-2xl border-white border-l-2 border-t-2 border-b-2 bg-[#0099cc] " + styles.darkshadow}>
-                <div className="font-semibold size-xl text-white text-2xl text-center my-[32px] mx-[64px]">
+                <div className="font-semibold size-xl text-white text-2xl text-center mt-[32px] mx-[64px]">
                     People who I think are very cool
                 </div>
+                <div className="size-sm text-white text-sm text-center mb-[32px] mx-[64px]"> In the order when I have first met them B)</div>
                 <div className="mx-[32px] grid sm:grid-cols-3 gap-2 items-center justify-items-center mb-[32px] h-[450px] overflow-y-scroll">
-                    {frens.reverse().map(({ url, img, name }) => <FriendItem url={url} img={img} name={name} />)}
+                    {frens.map(({ url, img, name }) => <FriendItem url={url} img={img} name={name} />)}
                 </div>
             </div>
         </div>
