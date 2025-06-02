ce43ec6b7a8abadd8bc8ad347392bbc6f0ec82ba
Jan Garong <jan.garong@mail.utoronto.ca>
2025-06-02 01:34:15 -0700

    in V1, add eslint disable.

--- src/lib/archive.ts
+++ src/lib/archive.ts
@@ -6,6 +6,7 @@ export function archiveExists() {
         const filePath = path.join(process.cwd(), 'public', 'b', 'archive.html')
         const encodedContent = fs.readFileSync(filePath, 'utf-8')
         return encodedContent != null
+    // eslint-disable-next-line 
     } catch (_) {
         return false
     }
