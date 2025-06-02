95fd32b7ddfc0d3446cd7930ca919e96b63a8781
Jan Garong <jan.garong@mail.utoronto.ca>
2025-06-02 01:31:14 -0700

    [V1] Add trycatch.

--- src/lib/archive.ts
+++ src/lib/archive.ts
@@ -2,7 +2,11 @@ import fs from 'fs'
 import path from 'path'
 
 export function archiveExists() {
-  const filePath = path.join(process.cwd(), 'public', 'b', 'archive.html')
-  const encodedContent = fs.readFileSync(filePath, 'utf-8')
-  return encodedContent != null
+    try {
+        const filePath = path.join(process.cwd(), 'public', 'b', 'archive.html')
+        const encodedContent = fs.readFileSync(filePath, 'utf-8')
+        return encodedContent != null
+    } catch (_) {
+        return false
+    }
 }
