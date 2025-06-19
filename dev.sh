96e31c39daaa61a1f239b10be673ae081b2df7dc
Jan Garong <jan.garong@mail.utoronto.ca>
2025-06-18 21:01:31 -0700

    fix script and post

--- dev.sh
+++ dev.sh
@@ -1,8 +1,15 @@
 # build docker image
 docker build -t jangarong/jangarong.github.io:4.1.0 .
+# # remove volumes
+# rm -rf volumes
+# remove posts and assets
+rm -rf ./volumes/assets
+rm -rf ./volumes/posts
 # should just be mdx files being copied
 cp ./posts/* ./volumes/posts
 # asset tree for b/log
 cp -r ./public/assets/b/log/* ./volumes/assets
+# # add archive
+# cp -r ./public/b/archive/* ./volumes/archive
 # spin up docker
 docker compose up -d
