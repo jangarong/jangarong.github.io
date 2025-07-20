7aef3d4a3509878eb2f7094628aa0824b96b1b40
Jan Garong <jan.garong@mail.utoronto.ca>
2025-07-20 16:34:45 -0700

    add v0 and v1

---
+++ public/b/alts/v1/precache-manifest.5b92ad006c8a66d80c6ca72296307295.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc871dba30c7a1c81adc84d7e2ef252",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ac53407f41716491317",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70558648720365324c71",
+    "url": "./static/js/2.2c20ad41.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.2c20ad41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac53407f41716491317",
+    "url": "./static/js/main.f395b2a0.chunk.js"
+  },
+  {
+    "revision": "9809b340e0a0130e3e7e",
+    "url": "./static/js/runtime-main.33dfd6b5.js"
+  },
+  {
+    "revision": "40feab090425383a7ad406724e269d06",
+    "url": "./static/media/cyc.40feab09.jpg"
+  },
+  {
+    "revision": "9815c0d317a8f1fc8afdfbb71fc73302",
+    "url": "./static/media/grr-pi.9815c0d3.jpg"
+  },
+  {
+    "revision": "a9336369b9fef6afc9d73988101be4f4",
+    "url": "./static/media/stockmarket.a9336369.jpg"
+  }
+]);
