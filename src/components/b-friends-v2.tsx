1ac53f53d833001c737a085a7988996d2467a889
Jan Garong <jan.garong@alumni.utoronto.ca>
2025-09-04 22:27:27 -0700

    update wording

--- src/components/b-friends-v2.tsx
+++ src/components/b-friends-v2.tsx
@@ -104,7 +104,7 @@ export default function BFriendsV2() {
                     <div className="font-semibold size-xl text-2xl text-center mt-[32px] mx-[64px]">
                         People who I think are very cool
                     </div>
-                    <div className="size-sm text-sm text-center mb-[32px] mx-[64px]">includes friends who I have and would like working with again B)</div>
+                    <div className="size-sm text-sm text-center mb-[32px] mx-[64px]">list of people who have been great coworkers or longtime friends of mine</div>
                     <div className="mx-[32px] grid sm:grid-cols-3 gap-2 items-center justify-items-center mb-[32px] h-[450px] overflow-y-scroll">
                         {frens.map(({ url, img, name }) => <FriendItem url={url} img={img} name={name} key={name} />)}
                     </div>
