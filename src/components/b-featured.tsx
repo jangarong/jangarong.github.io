ad9d297c8914b7c74b031847b6f4ac9080f1eae7
Jan Garong <jan.garong@alumni.utoronto.ca>
2025-11-16 13:13:39 -0800

    fix lint and update version and keep home page

--- src/components/b-featured.tsx
+++ src/components/b-featured.tsx
@@ -43,7 +43,7 @@ export default function BFeatured() {
                     This was co-created with Amy Li who led the art direction!
                 </div>
                 <div className="mt-[16px]">
-                    <Link href="/b/projects/zodiactail">Play now!</Link>
+                    <Link href="/b/rojects/zodiactail">Play now!</Link>
                 </div>
             </BFeaturedItem>
             {/* <BFeaturedItem src="/assets/b/solidguard.png" title="SolidGuard">
