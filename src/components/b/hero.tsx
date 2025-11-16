bf3e2c011626fadc585c516a56000e31a211726b
Jan Garong <jan.garong@alumni.utoronto.ca>
2025-11-16 10:05:45 -0800

    update banner

--- src/components/b/hero.tsx
+++ src/components/b/hero.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-
+import styles from "./hero.module.css"
 
 export default function Hero() {
     return (
-        <div className="base-banner pt-[48px] flex flex-row justify-between w-[100%]">
-            <div className="flex flex-col align-center justify-end">
+        <div className={"pt-[48px] flex flex-row justify-between w-[100%] " + styles.hero}>
+            {/* <div className="flex flex-col align-center justify-end">
                 <div className="flex flex-col align-center justify-center w-[250px] p-[24px]">
                     <Image src="/assets/b/jang-penguin.png" alt="JanG." width={250} height={50} />
                     <div className="text-center mt-[4px]">coding penguins</div>
@@ -12,6 +12,18 @@ export default function Hero() {
             </div>
             <div className="hidden sm:block flex flex-col align-center justify-end">
                 <Image src="/assets/b/pengdesk.png" alt="peng" className="mr-[32px]" width={250} height={100} />
+            </div> */}
+            <div className={styles.logoOuterContainer}>
+                <div className={styles.logoInnerContainer}>
+                    <Image src="/assets/b/jang-penguin.png" alt="JanG." width={250} height={50} />
+                </div>
+            </div>
+            <div className={styles.navbarOuterContainer}>
+                <div className={styles.navbarInnerContainer}>
+                    <div className={styles.navbar}>
+                        What's New | Projects | Sketches
+                    </div>
+                </div>
             </div>
         </div>
     )
