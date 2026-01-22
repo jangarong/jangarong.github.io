1b5b4fe639a433c571d4b3ba83ab13ce19247d3d
Jan Garong <jan.garong@alumni.utoronto.ca>
2026-01-21 23:59:21 -0800

    remove unused imports.

--- src/app/(card)/page.tsx
+++ src/app/(card)/page.tsx
@@ -1,15 +1,14 @@
 import JangCardMobileFooter from "@/components/jang-card-mobile-footer";
 import JangCardMobileHeader from "@/components/jang-card-mobile-header";
 import RGBCardMobile from "@/components/rgb-card-mobile";
-import Slide, { SlideDirection } from "@/components/slide";
 import { TheWorld } from "@/components/the-world";
 
 export default function Home() {
   return (
-      <RGBCardMobile className="" header={<JangCardMobileHeader />} body={
-        <div className="dark:invert">
-          <TheWorld />
-        </div>
-      } footer={<JangCardMobileFooter />} width={350} height={600} />
+    <RGBCardMobile className="" header={<JangCardMobileHeader />} body={
+      <div className="dark:invert">
+        <TheWorld />
+      </div>
+    } footer={<JangCardMobileFooter />} width={350} height={600} />
   );
 }
